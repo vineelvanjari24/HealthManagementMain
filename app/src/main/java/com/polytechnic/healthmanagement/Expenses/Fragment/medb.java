721e449b4e5ae9e69c8045ee28201b1eaa6a0c09
Sai Vignesh <021saivignesh@gmail.com>
2024-01-20 20:45:51 +0530

    Medical Expense Functionality Completed.

---
+++ app/src/main/java/com/polytechnic/healthmanagement/Expenses/Fragment/medb.java
@@ -0,0 +1,94 @@
+package com.polytechnic.healthmanagement.Expenses.Fragment;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.polytechnic.healthmanagement.TrackYourHealth.Model.TYHTable;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class medb extends SQLiteOpenHelper {
+    public static final String TABLENAME="ExpensesRecords";
+    public static final String DBNAME="MedicalExpenses";
+    public static final int DBVERSION=1;
+    public medb(@Nullable Context context) {
+        super(context, DBNAME, null, DBVERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TABLENAME+"(Id Integer PRIMARY KEY AUTOINCREMENT,Title text,doctorFee Integer,MedicinesCost Integer,TransCost Integer,Others Integer)");
+    }
+
+    public void addRecord(meNewRecord mnr){
+        SQLiteDatabase db=this.getReadableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put("Title",mnr.title);
+        cv.put("doctorFee",mnr.docfee);
+        cv.put("MedicinesCost",mnr.medexp);
+        cv.put("TransCost",mnr.trans);
+        cv.put("Others",mnr.other);
+        db.insert(TABLENAME,null,cv);
+    }
+
+    public ArrayList<meNewRecord> readRecords(){
+        ArrayList<meNewRecord> arr=new ArrayList<>();
+        SQLiteDatabase db=this.getWritableDatabase();
+        Cursor cur=db.rawQuery("SELECT * FROM "+TABLENAME,null);
+        while(cur.moveToNext()){
+            meNewRecord mnr=new meNewRecord();
+            mnr.title=cur.getString(1);
+            mnr.docfee=cur.getInt(2);
+            mnr.medexp=cur.getInt(3);
+            mnr.trans=cur.getInt(4);
+            mnr.other=cur.getInt(5);
+            arr.add(mnr);
+        }
+        return arr;
+    }
+
+    public int getMeId(int position){
+        int Id=0;
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor cur=db.rawQuery("Select * from "+TABLENAME,null);
+        while(position>0){
+            cur.moveToNext();
+            position--;
+        }
+        if(cur.moveToNext()){
+            Id=cur.getInt(0);
+        }
+        cur.close();
+        db.close();
+        return Id;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLENAME);
+        onCreate(db);
+    }
+
+    public void deleteMeRecord(int id) {
+    SQLiteDatabase db=this.getWritableDatabase();
+    db.delete(TABLENAME,"Id=?",new String[]{String.valueOf(id)});
+    }
+
+    public void updateMeRecord(meNewRecord mnr,int id) {
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues cv=new ContentValues();
+        cv.put("Title",mnr.title);
+        cv.put("doctorFee",mnr.docfee);
+        cv.put("MedicinesCost",mnr.medexp);
+        cv.put("TransCost",mnr.trans);
+        cv.put("Others",mnr.other);
+        db.update(TABLENAME,cv,"Id= ?",new String[]{String.valueOf(id)});
+    }
+}
---
+++ app/src/main/java/com/polytechnic/healthmanagement/Expenses/RecycleView/meRecycleView.java
@@ -0,0 +1,212 @@
+package com.polytechnic.healthmanagement.Expenses.RecycleView;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.polytechnic.healthmanagement.Expenses.Fragment.meNewRecord;
+import com.polytechnic.healthmanagement.Expenses.Fragment.medb;
+import com.polytechnic.healthmanagement.R;
+import com.polytechnic.healthmanagement.TrackYourHealth.Fragment.Ailment;
+import com.polytechnic.healthmanagement.TrackYourHealth.Fragment.tyhDB;
+import com.polytechnic.healthmanagement.TrackYourHealth.Model.TYHTable;
+import com.polytechnic.healthmanagement.TrackYourHealth.RecycleView.AilmentsList;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class meRecycleView extends RecyclerView.Adapter<meRecycleView.meViewHolder> {
+    ArrayList<meNewRecord> mearr=new ArrayList<>();
+    public meRecycleView(Context ct){
+        load(ct);
+    }
+    public void load(Context ct){
+        medb db=new medb(ct);
+        mearr=db.readRecords();
+        notifyDataSetChanged();
+    }
+    class meViewHolder extends RecyclerView.ViewHolder{
+        TextView title,amount,date;
+        ImageView im;
+        CardView cv;
+        public meViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title=itemView.findViewById(R.id.me_records_title);
+            amount=itemView.findViewById(R.id.me_records_amount);
+            date=itemView.findViewById(R.id.me_records_date);
+            im=itemView.findViewById(R.id.me_records_menu);
+            cv=itemView.findViewById(R.id.me_records_cardView);
+        }
+    }
+    @NonNull
+    @Override
+    public meViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater lf=LayoutInflater.from(parent.getContext());
+        View v=lf.inflate(R.layout.me_records_layout,parent,false);
+        return new meViewHolder(v);
+    }
+    @Override
+    public int getItemCount() {
+        return mearr.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull meViewHolder holder, int position) {
+        holder.title.setText((mearr.get(position)).title);
+        int amt=(mearr.get(position).docfee+mearr.get(position).other+mearr.get(position).medexp+mearr.get(position).trans);
+        holder.amount.setText(""+amt);
+        String cusdate=new SimpleDateFormat("YYYY-MM-DD").format(new Date());
+        holder.date.setText(cusdate);
+        holder.im.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                medb db=new medb(v.getContext());
+                int Id=db.getMeId(position);
+                showAilmentMenu(v,Id,mearr.get(position));
+            }
+        });
+        holder.cv.setOnClickListener(new View.OnClickListener() {
+            TextView tv1,tv2,tv3,tv4,tv5,tv6;
+            ImageView close;
+            @Override
+            public void onClick(View v) {
+                Dialog see=new Dialog(v.getContext());
+                see.setContentView(R.layout.me_desc_dialog);
+                tv1=see.findViewById(R.id.me_desc_dialog_tv1);
+                tv2=see.findViewById(R.id.me_desc_dialog_tv2);
+                tv3=see.findViewById(R.id.me_desc_dialog_tv3);
+                tv4=see.findViewById(R.id.me_desc_dialog_tv4);
+                tv5=see.findViewById(R.id.me_desc_dialog_tv5);
+                tv6=see.findViewById(R.id.me_desc_dialog_tv6);
+                close=see.findViewById(R.id.me_desc_dialog_close);
+                tv1.setText((mearr.get(position).title));
+                tv2.setText(String.valueOf((mearr.get(position).docfee)));
+                tv3.setText(String.valueOf((mearr.get(position).trans)));
+                tv4.setText(String.valueOf((mearr.get(position).medexp)));
+                tv5.setText(String.valueOf((mearr.get(position).other)));
+                tv6.setText(String.valueOf(amt));
+                see.show();
+                close.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        see.dismiss();
+                    }
+                });
+
+            }
+        });
+    }
+
+
+    public void showAilmentMenu(View v,  int Id, meNewRecord menew){
+        PopupMenu popupMenu = new PopupMenu(v.getContext(),v);
+        popupMenu.getMenuInflater().inflate(R.menu.tyh_options, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            EditText Title,medexp,docfee,others,trans;
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                CharSequence title = item.getTitle();
+                if (title.equals("Edit")) {
+                    Dialog editMeRecord = new Dialog(v.getContext());
+                    editMeRecord.setCancelable(false);
+                    editMeRecord.setContentView(R.layout.me_new_record_dialog);
+                    Button mecancel = editMeRecord.findViewById(R.id.me_dialog_cancel);
+                    Button mesave = editMeRecord.findViewById(R.id.me_dialog_save);
+                    Title=editMeRecord.findViewById(R.id.me_dialog_title);
+                    medexp=editMeRecord.findViewById(R.id.me_dialog_medcost);
+                    docfee=editMeRecord.findViewById(R.id.me_dialog_docfee);
+                    trans=editMeRecord.findViewById(R.id.me_dialog_trans);
+                    others=editMeRecord.findViewById(R.id.me_dialog_others);
+                    Title.setText(menew.title);
+                    medexp.setText(String.valueOf(menew.medexp));
+                    docfee.setText(String.valueOf(menew.docfee));
+                    trans.setText(String.valueOf(menew.trans));
+                    others.setText(String.valueOf(menew.other));
+                    editMeRecord.show();
+                    mecancel.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            editMeRecord.dismiss();
+                            Toast.makeText(v.getContext(), "Edit Cancled", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    mesave.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            meNewRecord mnr=new meNewRecord();
+                            if(Title.getText().toString().trim().equals("")){
+                                editMeRecord.dismiss();
+                                Toast.makeText(v.getContext(), "Title can't be empty", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                mnr.title=Title.getText().toString().trim();
+                                if(docfee.getText().toString().trim().equals(""))
+                                    mnr.docfee=0;
+                                else
+                                    mnr.docfee=Integer.parseInt(String.valueOf(docfee.getText()));
+                                if(trans.getText().toString().trim().equals(""))
+                                    mnr.trans=0;
+                                else
+                                    mnr.trans=Integer.parseInt(String.valueOf(trans.getText()));
+                                if(medexp.getText().toString().trim().equals(""))
+                                    mnr.medexp=0;
+                                else
+                                    mnr.medexp=Integer.parseInt(String.valueOf(medexp.getText()));
+                                if(others.getText().toString().trim().equals(""))
+                                    mnr.other=0;
+                                else
+                                    mnr.other=Integer.parseInt(String.valueOf(others.getText()));
+                                medb db = new medb(v.getContext());
+                                db.updateMeRecord(mnr, Id);
+                                editMeRecord.dismiss();
+                                load(v.getContext());
+                            }
+                        }
+                    });
+                    return true;
+                }else if (title.equals("Delete")) {
+                    AlertDialog.Builder confirm=new AlertDialog.Builder(v.getContext());
+                    confirm.setTitle("Delete Confirmation");
+                    confirm.setMessage("Do you want to delete for sure?");
+                    confirm.setCancelable(false);
+                    confirm.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            medb db=new medb(v.getContext());
+                            db.deleteMeRecord(Id);
+                            load(v.getContext());
+                        }
+                    });
+                    confirm.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(v.getContext(), "Delete Canceled",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    confirm.show();
+                    return true;
+                }
+                return true;
+            }
+        });
+        popupMenu.show();
+    }
+
+}
